e672a6d8f24f361ad6500bfa0f6805b595134bec
Yanchen <fengyc2018@gmail.com>
2018-10-24 20:22:06 -0400

    quote-machine

--- src/App.js
+++ src/App.js
@@ -1,28 +1,65 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {quote: {
+      content: '',
+      author: ''
+    }};
+    this.handleNewQuote = this.handleNewQuote.bind(this);
+    this.handleTweete = this.handleTweete.bind(this);
+  }
+
+  componentDidMount() {
+    this.handleNewQuote();
+  }
+
+  handleNewQuote() {
+    fetch('https://talaikis.com/api/quotes/random/')
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      const quoteData = data;
+      const { quote } = this.state;
+      quote.content = quoteData.quote;
+      quote.author = quoteData.author;
+      this.setState({quote});
+    });
+  }
+
+  handleTweete() {
+    window.open(`https://twitter.com/intent/tweet?text="${this.state.quote.content}"-${this.state.quote.author}`);
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App container" id='quote-box' >
+      <blockquote className='text-center my-5'>
+        <QuoteText content={this.state.quote.content}/> 
+        <QuoteAuthor author={this.state.quote.author} />
+      </blockquote>
+      <div className='mb-5 mx-auto btn-group' role='group'>
+        <button id='new-quote' className='btn btn-primary' onClick={this.handleNewQuote}>New quote</button>
+        <button id='new-quote' className='btn btn-primary' onClick={this.handleTweete}>Tweet</button>
+      </div>
+
       </div>
     );
   }
+}
+
+const QuoteText = (props) => {
+  return (<div id='text'>
+     {props.content}
+  </div>);
+}
+
+const QuoteAuthor = (props) => {
+  return (<div id='author'>
+    -{props.author}
+  </div>);
 }
 
 export default App;
